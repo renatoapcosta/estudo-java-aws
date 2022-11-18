534c628106f724c3f9c321f6c3199004922db6b2
renatoapcosta <renatoapcosta@gmail.com>
2022-11-18 00:43:29 -0300

    initial project

---
+++ src/main/java/br/com/ractecnologia/estudoaws/JavaAwsApplication.java
@@ -0,0 +1,13 @@
+package br.com.ractecnologia.estudoaws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaAwsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaAwsApplication.class, args);
+    }
+
+}
